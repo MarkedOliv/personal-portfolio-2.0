e5f28736a2a94f6ce96f4e0c62164e9110933dfc
MarkedOliv <olivdev4@gmail.com>
2023-11-23 16:39:32 -0400

    About section basics desktop done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LayoutComponent } from './components/Layout/layout.component';
 import { HeaderComponent } from './components/Header/header.component';
 import { HamburgerMenuComponent } from './components/Hamburger-Menu/hamburgerMenu.component';
 import { ProfileComponent } from './components/Profile/profile.component';
+import { AboutComponent } from './components/About/about.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +16,7 @@ import { ProfileComponent } from './components/Profile/profile.component';
     HamburgerMenuComponent,
     LayoutComponent,
     ProfileComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
