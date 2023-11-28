5c858d21b528103da4326969ce1dc7017b4af832
MarkedOliv <olivdev4@gmail.com>
2023-11-28 14:08:36 -0400

    Experience and Projects section basics done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HeaderComponent } from './components/Header/header.component';
 import { HamburgerMenuComponent } from './components/Hamburger-Menu/hamburgerMenu.component';
 import { ProfileComponent } from './components/Profile/profile.component';
 import { AboutComponent } from './components/About/about.component';
+import { ExperienceComponent } from './components/Experience/experience.component';
+import { ProjectsComponent } from './components/Projects/projects.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,6 +19,8 @@ import { AboutComponent } from './components/About/about.component';
     LayoutComponent,
     ProfileComponent,
     AboutComponent,
+    ExperienceComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/Experience/experience.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'experience',
+  templateUrl: 'experience.component.html',
+  styleUrls: ['./experience.component.css'],
+})
+export class ExperienceComponent {
+  moveInside(link: string) {
+    location.href = link;
+  }
+}
